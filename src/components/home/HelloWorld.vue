c262493ff3672a630f5a8729a629fe993a29cf6a
C1oudust <amerkiousy@gmail.com>
2020-04-07 19:44:34 +0800

    add home header

--- src/components/home/HelloWorld.vue
+++ src/components/home/HelloWorld.vue
@@ -41,17 +41,47 @@ export default {
 		width: 100vw;
 		height: 50px;
 		background: @bg-color;
-		box-shadow: 0px 1px 0px 0px rgba(0, 0, 0, 0.1);
+		border-bottom: solid 1px @border-color;
+		padding-left: @spacing-col-base;
+		padding-right: @spacing-col-base;
+
 		.left {
 			float: left;
 			img {
-				.img-size-sm();
+				margin-top: 9px;
+				.img-size-sm(); // 32px
 				border-radius: @border-radius-sm;
 			}
 		}
 		.center {
+			position: absolute;
+			text-align: center;
+			top: 0;
+			left: 0;
+			bottom: 0;
+			right: 0;
+
+			margin: auto;
+			img {
+				width: 44px;
+				height: 22px;
+				margin-top: 14px;
+			}
 		}
 		.right {
+			float: right;
+			height: 100%;
+			.search {
+				display: inline-block;
+			}
+			.add {
+				display: inline-block;
+			}
+			img {
+				.img-size-xs();
+				margin-right: 10px;
+				margin-top: 13px;
+			}
 		}
 	}
 }
