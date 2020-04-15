6dcccbd1a79029d7c72d34de363af159b7f714c5
C1oudust <amerkiousy@gmail.com>
2020-04-16 01:35:46 +0800

    finish  user detail page

--- src/views/UserDetail.vue
+++ src/views/UserDetail.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class="container">
+	<div class="container" @click="closePanel">
 		<div class="top-bar">
 			<div class="left">
 				<img src="../assets/images/detail/back.png" alt />
@@ -7,7 +7,7 @@
 
 			<div class="right">
 				<!-- <router-link to="/More"> -->
-				<div class="more">
+				<div v-show="!addFriend" class="more">
 					<img src="../assets/images/detail/more.png" alt />
 				</div>
 				<!-- </router-link> -->
@@ -19,20 +19,46 @@
 				<img src="static/img/one.jpg" alt />
 			</div>
 			<div class="user">
-				<div class="user-img">
-					<img class="img" src="static/img/one.jpg" alt />
-					<div class="sex" :style="{background:sexColor}">
-						<img src="../assets/images/detail/male.png" alt />
+				<transition name="fade">
+					<div v-show="!add">
+						<div class="user-img">
+							<div class="userimg">
+								<img class="img" src="static/img/one.jpg" alt />
+								<div class="sex" :style="{background:sexColor}">
+									<img src="../assets/images/detail/male.png" alt />
+								</div>
+							</div>
+						</div>
+						<div class="nick">{{user.nick}}</div>
+						<p class="remark" v-if="sendMsg" v-text="'('+user.remark+')'"></p>
+						<p class="content">{{user.content}}</p>
 					</div>
-				</div>
-				<div class="nick">{{user.nick}}</div>
-				<p class="remark" v-if="sendMsg" v-text="'('+user.remark+')'"></p>
-				<p class="content">{{user.content}}</p>
-				<div class="button add" v-if="addFriend">加好友</div>
+				</transition>
+				<div class="button add" @click="handleAddFriend" v-if="addFriend">加好友</div>
 				<div class="button edit" v-else-if="!isUser">编辑</div>
 				<div class="button msg" v-else-if="sendMsg">发消息</div>
+				<transition name="button-slide">
+					<div class="button send" @click="handleSendAdd" v-if="add">发送</div>
+				</transition>
 			</div>
 		</main>
+		<transition name="slide" mode="out-in">
+			<div id="add-friend" class="add-friend" v-if="add">
+				<div class="user-img">
+					<img class="img" src="static/img/one.jpg" alt />
+				</div>
+				<div class="nick">{{user.nick}}</div>
+				<textarea
+					class="add-content"
+					name="content"
+					id
+					cols="30"
+					rows="10"
+					v-text="unick+'请求加为好友'"
+					maxlength="198"
+				></textarea>
+			</div>
+		</transition>
 	</div>
 </template>
 
@@ -41,9 +67,13 @@ export default {
 	name: 'UserDetail',
 	data() {
 		return {
+			unick: '云尘',
 			sexColor: "rgb(34, 158, 216)",// rgb(255,93,91)
-			isUser: false,
+			isUser: true,
 			isFriend: false,
+			add: false,
+			userimg: 'userimg',
+			userimg2: 'userimg2',
 			user: {
 				nick: '云尘',
 				remark: '云尘',
@@ -58,14 +88,75 @@ export default {
 		},
 		sendMsg: function () {
 			return this.isUser && this.isFriend;
-		}
+		},
 
+	},
+	methods: {
+		handleAddFriend() {
+			this.add = true;
+		},
+		handleSendAdd() {
+
+
+		},
+		closePanel(e) {
+			if (this.add) {
+				var addPanel = document.getElementById('add-friend');
+				if (e.clientY < addPanel.offsetTop && e.clientY > 50) {
+					this.add = false;
+
+				}
+			}
+
+		}
 	}
+
 }
 </script>
 
 <style lang="less" scoped>
 @import '../common/style.less';
+
+.slide-enter-active {
+	animation: slideIn 0.5s;
+}
+
+.slide-leave-active {
+	animation: slideOut 0.5s forwards;
+}
+.button-slide-enter-active {
+	animation: slideIn 0.5s;
+}
+
+.button-slide-leave-active {
+	animation: slideIn 0.2s reverse;
+}
+
+@keyframes slideIn {
+	0% {
+		transform: translateY(100%);
+	}
+	100% {
+		transform: translateY(0);
+	}
+}
+@keyframes slideOut {
+	100% {
+		transform: translateY(100%);
+	}
+	0% {
+		transform: translateY(0);
+	}
+}
+.fade-enter-active,
+.fade-leave-active {
+	transition: opacity 0.5s;
+}
+.fade-enter,
+.fade-leave-to {
+	opacity: 0;
+}
+
 .container {
 	overflow: hidden;
 	.top-bar {
@@ -128,6 +219,7 @@ export default {
 			align-items: center;
 			.user-img {
 				position: relative;
+				transition: 0.5s;
 				.img {
 					width: 256px;
 					height: 256px;
@@ -151,7 +243,21 @@ export default {
 					}
 				}
 			}
-
+			// .userimg2 {
+			// 	position: absolute;
+			// 	transition: 0.5s;
+			// 	.img {
+			// 		width: 128px;
+			// 		height: 128px;
+			// 		border-radius: @border-radius-circle;
+			// 		box-sizing: content-box;
+			// 		border: 2px solid @bg-color;
+			// 		box-shadow: 4px 10px 20px 1px rgba(0, 0, 0, 0.3);
+			// 	}
+			// 	.sex {
+			// 		display: none;
+			// 	}
+			// }
 			.nick {
 				font-size: @font-size-lg;
 				margin-top: @spacing-col-lg;
@@ -190,6 +296,62 @@ export default {
 			.msg {
 				background: linear-gradient(90deg, #48c6ef 0%, #6f86d6 100%);
 				color: @text-color-inverse;
+			}
+			.send {
+				z-index: 2;
+				background: linear-gradient(90deg, #48c6ef 0%, #6f86d6 100%);
+				color: @text-color-inverse;
+			}
+		}
+	}
+
+	.add-friend {
+		transition: 2s;
+		position: fixed;
+		bottom: 0;
+		width: 100%;
+		height: 75vh;
+		border-radius: @border-radius-lg;
+		background-color: @bg-color;
+
+		display: flex;
+		justify-content: flex-start;
+		align-items: center;
+		flex-direction: column;
+		.user-img {
+			position: absolute;
+
+			top: 32px;
+			.img {
+				width: 128px;
+				height: 128px;
+				border-radius: @border-radius-circle;
+				box-sizing: content-box;
+				// border: 2px solid @bg-color;
+				box-shadow: 2px 5px 20px 1px rgba(0, 0, 0, 0.3);
+			}
+		}
+		.nick {
+			padding-top: 22vh;
+			margin-right: 65vw;
+			font-size: @font-size-lg;
+			color: @text-color;
+			line-height: 50px;
+		}
+		.add-content {
+			margin-top: 10px;
+			border: none;
+			border-radius: @border-radius-base;
+			background-color: rgba(241, 241, 241, 0.9);
+			width: 80%;
+			height: 32vh;
+			padding: 15px;
+			resize: none;
+			outline: none;
+			color: @text-color-grey;
+
+			&:focus {
+				color: @text-color;
 			}
 		}
 	}
